db5bb9401e420a222ef3a6a9dba901ed0b5ef1df
Marius Holter Berntzen <mhberntzen@gmail.com>
2026-01-19 14:49:20 +0100

    Update Homebrew formula for v1.0.2

--- Formula/morningweave.rb
+++ Formula/morningweave.rb
@@ -1,9 +1,9 @@
 class Morningweave < Formula
   desc "Single-user CLI that builds a scheduled content digest"
   homepage "https://github.com/mbtz/morningweave"
-  url "https://github.com/mbtz/morningweave/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "c9f616b00f78196f9c9d1f828bcb20707d64a91b5e8fe1b4b8d1975a5519cdbc"
-  version "1.0.1"
+  url "https://github.com/mbtz/morningweave/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "96659556c672e0e8001770c6a2ba9475bb9fcc03694c8f590ec130c876ec1a4b"
+  version "1.0.2"
   head "https://github.com/mbtz/morningweave.git", branch: "main"
 
   depends_on "go" => :build
